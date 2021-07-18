44e8430876b3f581af85634ddb6f1951c5633a39
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-07-18 22:46:37 +0100

    created a new file to handle server services

---
+++ frontend/src/APIService.js
@@ -0,0 +1,12 @@
+export default class APIService {
+    static InsertTask(body) {
+        return fetch('http://127.0.0.1:8000/api/todos/', {
+            'method': 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        })
+            .then(resp => resp.json())
+    }
+}
