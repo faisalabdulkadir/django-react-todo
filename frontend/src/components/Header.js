fe1ca56ac0509879fc484aca9814d0ed37a102ed
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-07-18 17:50:24 +0100

    refactor header into a component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import { BiListPlus } from 'react-icons/bi'
+
+function Header({toggleForm}) {
+
+  return (
+    <div className="header-section">
+      <header>Todo App</header>
+      <button
+        className="icon"
+        onClick={toggleForm}
+      >
+        <i className="fas fa-plus"><BiListPlus /></i>
+      </button>
+    </div>
+  )
+}
+
+export default Header
