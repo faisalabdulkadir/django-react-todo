74176e62be56c6f4fbc6b6b43efdef4d7321050f
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-07-18 17:53:54 +0100

    created toggle function for the form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,11 @@ import './css/styles.css'
 import Tasks from './components/Tasks';
 import Footer from './components/Footer';
 import Form from './components/Form';
+import Header from './components/Header';
 
 function App() {
   const [taskList, setTaskList] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     fetch('http://127.0.0.1:8000/api/todos', {
@@ -19,11 +21,16 @@ function App() {
       .then(response => setTaskList(response))
       .catch(error => console.log(error))
   }, [])
+
+  const toggleForm = () => {
+    setShowForm(!showForm)
+  }
+  
   return (
     <div>
       <div className="wrapper">
-        <header>Todo App</header>
-        <Form />
+       <Header toggleForm={toggleForm}/>
+        {showForm ? <Form />: ''}
         <Tasks taskList={taskList}/>
         <Footer taskList={taskList} />
       </div>
