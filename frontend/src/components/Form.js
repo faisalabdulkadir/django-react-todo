526bf879ede9ecf6492ad0ab4062d81297fa6a7a
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-07-18 17:55:27 +0100

    converted the form to a controlled form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { BiListPlus } from 'react-icons/bi'
+import React, { useState } from 'react'
 function Form() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+
   return (
-    <div>
-      <div className="inputField">
-        <input type="text" placeholder="Add your new todo" />
-        <button><i className="fas fa-plus"><BiListPlus /></i></button>
-      </div>
+    <div className="inputField">
+      <form>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          placeholder="Add your new title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="descripton"
+          rows="5"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <label htmlFor="date">Date</label>
+        <input
+          type="date"
+          id="date"
+          value={date}
+          onChange={e => setDate(e.target.value)}
+        />
+        <label htmlFor="time">Time</label>
+        <input
+          type="time"
+          id="time"
+          value={time}
+          onChange={e => setTime(e.target.value)}
+        />
+        <button
+          className="save-button"
+        >
+          Save Task
+        </button>
+      </form>
     </div>
   )
 }
