3e47cb76843a57c0dcae59520a6bb52195b2d1d6
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-07-25 00:37:44 +0100

    added function to open form on clicking the edit icon

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { MdDelete, MdEdit } from 'react-icons/md'
 
 
-function Tasks({taskList}) {
+function Tasks({ taskList, editButton }) {
+  const editBtn = (task) => {
+    editButton(task);
+  }
   return (
     <div>
       <ul className="todoList">
         {taskList.map((task) => (
           <li key={task.id}>{task.title}
 
-            <i className="icon-edit"><MdEdit /></i>
+            <i
+              className="icon-edit"
+              onClick={() => editBtn(task)}
+            >
+              <MdEdit />
+            </i>
             <i className="icon-delete"><MdDelete /></i>
           </li>
         ))}
