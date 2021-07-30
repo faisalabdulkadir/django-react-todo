64053cc09a150c567d5e4dd9b3e47a528877f10a
Faisal Abdulkadir <wizardfaisal@gmail.com>
2021-07-30 18:09:25 +0100

    minor changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,6 @@ function Footer({ taskList }) {
     <div>
       <div className="footer">
         <span>You have ( <span className="pendingTasks">{taskList.length}</span> ) pending tasks</span>
-        <button>Clear All</button>
       </div>
     </div>
   )
